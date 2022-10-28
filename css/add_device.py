51426d9016bbbc71c216a5089d471d9aef7a5bf5
shawon <helal.uddin2453@gmail.com>
2022-10-28 21:16:30 +0600

    C:\Users\vivo1\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe -m pip install --upgrade pipyp

---
+++ css/add_device.py
@@ -0,0 +1,43 @@
+import json
+import requests
+ids = [
+   '352625690688443',
+   '352625696254703',
+   '354018117382295',
+   '352625690693427',
+   '354018118117807',
+]
+
+
+url = "http://168.119.115.32:8082/api/devices"
+
+
+def add_device(serial, id):
+    payload = json.dumps({
+        "name": f"Vehicle {serial}",
+        "uniqueId": id,
+        "status": "OFF",
+        "disabled": False,
+        "lastUpdate": "2022-10-14T00:15:22Z",
+        "positionId": 0,
+        "groupId": 0,
+        "phone": "string",
+        "model": "string",
+        "contact": "string",
+        "category": "string",
+        "geofenceIds": [
+            0
+        ],
+        "attributes": {}
+    })
+    headers = {
+        'Authorization': 'Basic YWRtaW46YWRtaW4=',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(id, response.status_code)
+
+for i, v in enumerate(ids):
+    add_device(i+4, v)
